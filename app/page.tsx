397e6504158292fe5005377014901874e181d284
Sezer Karakus <karakuss.sezer@gmail.com>
2026-08-10 15:36:31 +0200

    Revert "Infrastruktur-Sektion mit Server-/Netzwerk-/Glasfaser-Fotos auf blauem Hintergrund"
    This reverts commit 9017e77a858cba3132aff034e2805be2cd47ae19.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import TrustBar from "@/components/TrustBar";
 import Services from "@/components/Services";
-import Infrastructure from "@/components/Infrastructure";
 import SecurityBand from "@/components/SecurityBand";
 import References from "@/components/References";
 import Cta from "@/components/Cta";
@@ -16,7 +15,6 @@ export default function Home() {
         <Hero />
         <TrustBar />
         <Services />
-        <Infrastructure />
         <SecurityBand />
         <References />
         <Cta />
